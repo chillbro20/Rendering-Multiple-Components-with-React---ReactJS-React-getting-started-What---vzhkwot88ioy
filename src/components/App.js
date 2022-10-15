3169c381c81ae2905b6ecc54ef930b771a4b8005
chillbro20 <97622677+chillbro20@users.noreply.github.com>
2022-10-15 10:57:19 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 import React, {Component, useState} from "react";
+// import '../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div id="main">
+//       <h1 data-ns-test="project-name"></h1>
+//       <p data-ns-test="project-description">
+
+//       </p>
+//     </div>
+//   )
+// }
+
+
+// export default App;
 import '../styles/App.css';
+import ProjectList from '../components/ProjectList';
 
-const App = () => {
+function App() {
   return (
-    <div id="main"></div>
-  )
+    <div className="App">
+     <ProjectList name="html" description="html project with e-commerce"/>
+     <ProjectList name="css" description="css project with e-commerce"/>
+     <ProjectList name="javsacript" description="javascript project with e-commerce"/>
+     <ProjectList name="reactjs" description="reactjs project with e-commerce"/>
+    </div>
+  );
 }
-
 
 export default App;
